84010b3b6940714ade0066709f8cf8ca28fa83e6
Justin Morris <justinjodymorris@gmail.com>
2018-06-19 14:59:10 -0700

    added webpack files

---
+++ webpack.dev.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const merge = require('webpack-merge');
+const commonConfig = require('./webpack.common.js');
+
+const {HotModuleReplacementPlugin} = require('webpack');
+
+const webpackDevConfig = {};
+webpackDevConfig.module = {};
+webpackDevConfig.mode = 'development';
+webpackDevConfig.devtool = 'inline-source-map';
+
+webpackDevConfig.devServer = {
+  contentBase: './build',
+  open: true,
+  hot: true,
+  historyApiFallback: true,
+};
+
+webpackDevConfig.plugins = [
+  new HotModuleReplacementPlugin(),
+];
+
+webpackDevConfig.module.rules = [
+  {
+    tes: /\.scss$/,
+    use: [
+      {
+        loader: 'style-loader',
+      },
+      {
+        loader: 'css-loader',
+        options: { sourceMap: true},
+      },
+      {
+        loader: 'sass-loader',
+        options: { sourceMap: true},
+      },
+    ],
+  },
+];
+
+module.exports = merge(commonConfig, webpackDevConfig);
