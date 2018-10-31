dfb4432d726eaf1065ca3f146ff8bf1886b944be
Ben Hurst <BHurst8@gmail.com>
2018-10-31 15:54:53 -0700

    Proof of life

--- src/components/app.js
+++ src/components/app.js
@@ -28,22 +28,24 @@ export default class App extends React.Component {
     e.preventDefault();
     let searchFormBoard = e.target.searchFormBoard.value.trim();
     let searchFormLimit = e.target.searchFormLimit.value;
-    let url = `http://www.reddit.com/r/${searchFormBoard}.json?limit=${searchFormLimit}`;
+    let url = `http://localhost:6543/api/v1/feed/`;
 
     superagent.get(url)
+      // .set('Authorization', 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJyb2xlcyI6WyJhZG1pbiJdLCJ1c2VyTmFtZSI6ImJlbm55Ym95MTIzNDU2QGdtYWlsLmNvbSIsInN1YiI6ImJlbm55Ym95MTIzNDU2QGdtYWlsLmNvbSIsImlhdCI6MTU0MDk1NDE4NH0.6zkAEbl0oKAE-fFGHL4BQ7wYioepk81qhiNsCQVgjRJx4zsNBBjRG2MOk6PWvtc9vNiBn6IWLKYEOA_JMSVNrA')
       .then(results => {
+        console.log(results);
         this.setState({hasError: false});
         let topics = results.body.data.children;
         this.setState(Object.assign(...this.state, {topics}));
       })
-      .catch(err => this.setState({hasError: true}));
+      .catch(err => console.log(err));
   }
 
   render() {
     return (
       <main>
         <div id="logo"><FontAwesomeIcon icon={['fab', 'reddit']} /></div>
-        <h1>What's Happening on the Web?</h1>
+        <h1>Intellinewz</h1>
         <SearchForm searchClass={this.state.hasError ? 'error' : 'success'} redditSearch={this.handleSubmit} />
         <ul>
           <SearchResultList searchResults={this.state.topics}/>
