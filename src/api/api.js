3ef170707182b676f3c3501d1223b859c6350e04
Justin Morris <justinjodymorris@gmail.com>
2018-11-02 05:31:59 -0700

    added ternary to allow for request of feed with no tones

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import superagent from 'superagent';
 
 export const retrieveFeed = async tones => {
-  let query = `?pref=${tones.join('+')}`;
+  let query = tones && tones.length ? `?pref=${tones.join('+')}` : '';
   let url = `${process.env.REACT_APP_API_URL}feed/${query}`;
   let res = await superagent.get(url);
   return res.body;
