b8cba6526c7dadd664c6714a8fde7de593ef4328
Justin Morris <justinjodymorris@gmail.com>
2018-11-06 07:38:47 -0800

    changed format of the article template

--- src/components/article/article.js
+++ src/components/article/article.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
 export default class Article extends React.Component {
+
+  formatTime = date => {
+    console.log(...date);
+  }
+
   render() {
     let article = this.props.article;
+    // let time = this.formatTime(new Date(article.date_published));
+
     return (
-      <article key={article.title}>
+      <article key={article.id}>
+        <img src={article.image} />
         <h2>{article.title}</h2>
-        <a href={article.url}>Src: {article.url}</a>
+        <p>{article.date_published}</p>
+        <p>{article.description}</p>
+        <a rel="noopener noreferrer" target="_blank" href={article.url}>Src: {article.url}</a>
       </article>
     );
   }
